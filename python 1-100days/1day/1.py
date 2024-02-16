8b27acdea489696cd8069ee48a82d0bcf9ea72c3
xiushi <xiaoqie428@qq.com>
2024-02-16 19:50:31 +0800

    update

---
+++ python 1-100days/1day/1.py
@@ -0,0 +1,17 @@
+import turtle # 导入turtle库
+turtle.pensize(4) # 设置画笔的大小
+turtle.pencolor('yellow') #
+
+turtle.forward(200) # 向前走200
+turtle.right(90) # 向右转90度
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.pencolor('red') # 设置画笔的颜色
+turtle.left(45) # 向左转45度
+turtle.forward(200) # 向前走200
+
+turtle.mainloop()
